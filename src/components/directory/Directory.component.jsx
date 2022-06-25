690ccc611b24a8a4779431ae31bc252910a88984
Asil <asil@null.com>
2022-06-25 13:40:14 +0300

    N: Categories directory added

---
+++ src/components/directory/Directory.component.jsx
@@ -0,0 +1,15 @@
+import './directory.styles.scss';
+import CategoryItem from '../category-item/CategoryItem.component';
+
+const Directory = ({ categories }) => {
+
+    return (
+        <div className="categories-container">
+            {categories.map((category) => (
+                <CategoryItem key={category.id} category={category} />
+            ))}
+        </div>
+    )
+}
+
+export default Directory
