a32de026363488166cbcae3332fde735dbbaf97f
mehmetasilkilic <mehmetasil96@gmail.com>
2022-07-08 20:05:23 +0300

    N: redux added

---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+import { compose, createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+
+import { rootReducer } from "./rootReducer";
+
+const middleWares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
