e6bf32a21f52927e3e390ca658d6a93eea38425c
mehmetasilkilic <mehmetasil96@gmail.com>
2022-07-11 16:11:02 +0300

    U: category reducer updated to async

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -2,6 +2,8 @@ import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
 export const CATEGORIES_INITIAL_STATE = {
   categoriesArray: [],
+  isLoading: false,
+  error: null,
 };
 
 export const categoriesReducer = (
@@ -9,10 +11,13 @@ export const categoriesReducer = (
   action = {}
 ) => {
   const { type, payload } = action;
-
   switch (type) {
-    case CATEGORIES_ACTION_TYPES.SET_CATEGORIES:
-      return { ...state, categoriesArray: payload };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
+      return { ...state, isLoading: true };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
+      return { ...state, categoriesArray: payload, isLoading: false };
+    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
+      return { ...state, error: payload, isLoading: false };
     default:
       return state;
   }
