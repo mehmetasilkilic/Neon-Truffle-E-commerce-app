964fad7b32889d7b06a31136b1eb37c1e81af234
mehmetasilkilic <mehmetasil96@gmail.com>
2022-07-18 11:16:17 +0300

    U: cartDropdown updated to open on mouse hover

--- src/components/cartIcon/CartIcon.component.jsx
+++ src/components/cartIcon/CartIcon.component.jsx
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 import {
   selectCartCount,
@@ -12,14 +13,20 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cartIcon.styles.scss";
 
 const CartIcon = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const cartCount = useSelector(selectCartCount);
   const isCartOpen = useSelector(selectIsCartOpen)
 
   const toggleIsCartOpen = () => dispatch(setIsCartOpen(!isCartOpen));
 
+  const goToCheckOutHandler = () => {
+    navigate("/checkout");
+  };
+
   return (
-    <div className="cart-icon-container" onClick={toggleIsCartOpen}>
+    <div className="cart-icon-container" onMouseEnter={toggleIsCartOpen}
+      onClick={goToCheckOutHandler}>
       <ShoppingIcon className="shopping-icon" />
       <span className="item-count">{cartCount}</span>
     </div>
