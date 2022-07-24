42e9410eb8b3989761af37b230f70dec88403503
mehmetasilkilic <mehmetasil96@gmail.com>
2022-07-24 10:25:04 +0300

    U: Button component coverted to tsx

---
+++ src/components/button/Button.component.tsx
@@ -0,0 +1,31 @@
+import { FC, ButtonHTMLAttributes } from "react";
+
+import "./button.styles.scss";
+
+export type ButtonProps = {
+  buttonType?: string;
+  isLoading?: boolean;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button: FC<ButtonProps> = ({
+  children,
+  isLoading,
+  buttonType,
+  ...otherProps
+}): JSX.Element => {
+  return (
+    <button
+      disabled={isLoading}
+      className={`button-container ${buttonType || ""}`}
+      {...otherProps}
+    >
+      {isLoading ? (
+        <div className="spinner-container"></div>
+      ) : (
+        <div>{children}</div>
+      )}
+    </button>
+  );
+};
+
+export default Button;
