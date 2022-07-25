6de6064a99c5c1743001bbb0f49e0426fbd964db
mehmetasilkilic <mehmetasil96@gmail.com>
2022-07-25 12:43:38 +0300

    U: ProductCard component coverted to tsx

--- src/components/productCard/ProductCard.component.tsx
+++ src/components/productCard/ProductCard.component.tsx
@@ -1,13 +1,19 @@
+import { FC } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { selectCartItems } from "../../store/cart/cart.selector";
 import { addItemToCart } from "../../store/cart/cart.action";
+import { CategoryItem } from "../../store/categories/category.types";
 
 import Button from "../button/Button.component";
 
 import "./productCard.styles.scss";
 
-const ProductCard = ({ product }) => {
+type ProductCardProps = {
+  product: CategoryItem;
+};
+
+const ProductCard: FC<ProductCardProps> = ({ product }) => {
   const { name, price, imageUrl } = product;
   const dispatch = useDispatch();
   const cartItems = useSelector(selectCartItems);
