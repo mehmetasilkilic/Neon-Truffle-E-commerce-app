22971aac6c7019c3d01905c9041901d68c64f188
mehmetasilkilic <mehmetasil96@gmail.com>
2022-07-25 12:22:32 +0300

    U: CategoryPreview component coverted to tsx

--- src/components/categoryPreview/CategoryPreview.component.tsx
+++ src/components/categoryPreview/CategoryPreview.component.tsx
@@ -1,10 +1,18 @@
+import { FC } from "react";
 import { Link } from "react-router-dom";
 
 import ProductCard from "../productCard/ProductCard.component";
 
+import { CategoryItem } from "../../store/categories/category.types";
+
 import "./categoryPreview.styles.scss";
 
-const CategoryPreview = ({ title, products }) => {
+type CategoryPreviewProps = {
+  title: string;
+  products: CategoryItem[];
+};
+
+const CategoryPreview: FC<CategoryPreviewProps> = ({ title, products }) => {
   return (
     <div className="category-preview-container">
       <h2>
