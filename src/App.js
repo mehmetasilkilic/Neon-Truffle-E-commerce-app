23306e6ca564c51a137267529bafb4e4755e6cef
mehmetasilkilic <mehmetasil96@gmail.com>
2022-07-25 17:19:56 +0300

    N: lazy & suspense add to split code at route lvl

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 
-import Navigation from "./routes/navigation/Navigation.component";
-import Home from "./routes/home/Home.component";
-import Authentication from "./routes/authentication/Authentication.component";
-import Shop from "./routes/shop/Shop.component";
-import Checkout from "./routes/checkout/Checkout.component";
+import Spinner from "./components/spinner/Spinner.component";
 import { checkUserSession } from "./store/user/user.action";
+
+const Navigation = lazy(() => import('./routes/navigation/Navigation.component'))
+const Home = lazy(() => import('./routes/home/Home.component'))
+const Authentication = lazy(() => import('./routes/authentication/Authentication.component'))
+const Shop = lazy(() => import('./routes/shop/Shop.component'))
+const Checkout = lazy(() => import('./routes/checkout/Checkout.component'))
 
 const App = () => {
   const dispatch = useDispatch();
@@ -17,14 +19,16 @@ const App = () => {
   }, []);
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
